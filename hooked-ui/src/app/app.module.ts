5c7c4a95e6b72dac485fe22fbaf1a85532ee63e1
AcHyUt1234 <achyut.dubey6.4@gmail.com>
2021-01-08 22:58:08 +0530

    Login/Register tab

--- hooked-ui/src/app/app.module.ts
+++ hooked-ui/src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
@@ -37,6 +38,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
